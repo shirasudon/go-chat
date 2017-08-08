3dd48ed5ec5d8a5e80651d79e5cc301b1a68a3a4
mzki <mk22.cc328@gmail.com>
2017-08-09 01:06:18 +0900

    model: add client_manager

---
+++ model/client_manager.go
@@ -0,0 +1,120 @@
+package model
+
+import "fmt"
+
+// connection specified infomation.
+type connectionInfo struct {
+	currentRoomID uint64
+}
+
+// activeClient is a one active user which has several
+// websocket connections, rooms, and friends.
+//
+// activeClient has multiple websocket conncetion,
+// because one user has many websocket conncetions such as from PC,
+// mobile device, and so on.
+type activeClient struct {
+	conns   map[*Conn]connectionInfo
+	rooms   map[uint64]bool // has rooms managed by room id
+	friends map[uint64]bool // has friends managed by user id
+}
+
+func newActiveClient(c *Conn) *activeClient {
+	return &activeClient{
+		conns:   map[*Conn]connectionInfo{c: connectionInfo{}},
+		rooms:   make(map[uint64]bool),
+		friends: make(map[uint64]bool),
+	}
+}
+
+func (ac *activeClient) Send(m ActionMessage) {
+	for c, _ := range ac.conns {
+		c.Send(m)
+	}
+}
+
+// ClientManager manages active clients.
+type ClientManager struct {
+	clients map[uint64]*activeClient
+}
+
+func NewClientManager() *ClientManager {
+	return &ClientManager{
+		clients: make(map[uint64]*activeClient),
+	}
+}
+
+func (cm *ClientManager) broadcastsFriends(ac *activeClient, m ActionMessage) {
+	for friendID, _ := range ac.friends {
+		if activeFriend, ok := cm.clients[friendID]; ok {
+			activeFriend.Send(m)
+		}
+	}
+}
+
+func (cm *ClientManager) connectClient(c *Conn) {
+	// add conncetion to active user when the user is already connected from anywhere.
+	if activeC, ok := cm.clients[c.userID]; ok {
+		activeC.conns[c] = connectionInfo{}
+		return
+	}
+
+	// create new active client because the connection is newly.
+	// and broadcasts user connect event to all active friends
+	activeC := newActiveClient(c)
+	// TODO set friends and rooms to active user.
+	cm.clients[c.userID] = activeC
+
+	uc := UserConnect{} // TODO implement UserConncet.
+	cm.broadcastsFriends(activeC, uc)
+}
+
+// NoUserID is nerver used as user id.
+const NoUserID = 0
+
+func (cm *ClientManager) roomIDFromConn(c *Conn) uint64 {
+	activeC, ok := cm.clients[c.userID]
+	if !ok {
+		return NoUserID
+	}
+	connInfo, ok := activeC.conns[c]
+	if !ok {
+		return NoUserID
+	}
+	return connInfo.currentRoomID
+}
+
+func (cm *ClientManager) disconnectClient(c *Conn) {
+	activeC, ok := cm.clients[c.userID]
+	if !ok {
+		return
+	}
+
+	delete(activeC.conns, c)
+	if len(activeC.conns) == 0 {
+		delete(cm.clients, c.userID)
+		// udc := UserDisconnect{} // TODO implement UserDisconncet.
+		// cm.broadcastsFriends(activeC, udc)
+	}
+}
+
+func (cm *ClientManager) validateClientHasRoom(conn *Conn, userID, roomID uint64) error {
+	if !cm.connectionExist(userID, conn) {
+		return fmt.Errorf("request user id(%d) is not found", userID)
+	}
+	// the client is validated at above.
+	if _, ok := cm.clients[userID].rooms[roomID]; !ok {
+		return fmt.Errorf("user(%d) does not have request room id(%d) ", userID, roomID)
+	}
+	return nil
+}
+
+// check whether active client with the websocket connection exists?
+func (cm *ClientManager) connectionExist(userID uint64, conn *Conn) bool {
+	if activeC, ok := cm.clients[userID]; ok {
+		if _, ok := activeC.conns[conn]; ok {
+			return true
+		}
+	}
+	return false
+}
--- model/initial_room.go
+++ model/initial_room.go
@@ -2,43 +2,11 @@ package model
 
 import (
 	"context"
-	"fmt"
 	"log"
 
 	"github.com/shirasudon/go-chat/entity"
 	"golang.org/x/net/websocket"
 )
-
-// connection specified infomation.
-type connectionInfo struct {
-	currentRoomID uint64
-}
-
-// activeClient is a one active user which has several
-// websocket connections, rooms, and friends.
-//
-// activeClient has multiple websocket conncetion,
-// because one user has many websocket conncetions such as from PC,
-// mobile device, and so on.
-type activeClient struct {
-	conns   map[*Conn]connectionInfo
-	rooms   map[uint64]bool // has rooms managed by room id
-	friends map[uint64]bool // has friends managed by user id
-}
-
-func newActiveClient(c *Conn) *activeClient {
-	return &activeClient{
-		conns:   map[*Conn]connectionInfo{c: connectionInfo{}},
-		rooms:   make(map[uint64]bool),
-		friends: make(map[uint64]bool),
-	}
-}
-
-func (ac *activeClient) Send(m ActionMessage) {
-	for c, _ := range ac.conns {
-		c.Send(m)
-	}
-}
 
 // InitialRoom is the room which have any clients created newly.
 // any clients enters this room firstly, then dispatch their to requesting rooms.
@@ -52,8 +20,8 @@ type InitialRoom struct {
 	messages    chan ActionMessage
 
 	// TODO RoomRepository to accept that correct user enters a room.
-	roomManager *RoomManager
-	clients     map[uint64]*activeClient
+	rooms   *RoomManager
+	clients *ClientManager
 }
 
 func NewInitialRoom( /*rRepo RoomRepository*/ ) *InitialRoom {
@@ -61,8 +29,8 @@ func NewInitialRoom( /*rRepo RoomRepository*/ ) *InitialRoom {
 		connects:    make(chan *Conn, 1),
 		disconnects: make(chan *Conn, 1),
 		messages:    make(chan ActionMessage, 1),
-		roomManager: NewRoomManager( /*rRepo*/ ),
-		clients:     make(map[uint64]*activeClient),
+		rooms:       NewRoomManager( /*rRepo*/ ),
+		clients:     NewClientManager(),
 	}
 }
 
@@ -73,37 +41,18 @@ func (iroom *InitialRoom) Listen(ctx context.Context) {
 	for {
 		select {
 		case c := <-iroom.connects:
-			iroom.connectClient(c)
+			iroom.clients.connectClient(c)
+
 		case c := <-iroom.disconnects:
-			iroom.disconnectClient(c)
+			roomID := iroom.clients.roomIDFromConn(c)
+			iroom.rooms.DisconnectClient(roomID, c)
+			iroom.clients.disconnectClient(c)
+
 		case m := <-iroom.messages:
 			iroom.handleMessage(ctx, m)
+
 		case <-ctx.Done():
 			return
-		}
-	}
-}
-
-func (iroom *InitialRoom) connectClient(c *Conn) {
-	// add conncetion to active user when the user is already connected from anywhere.
-	if activeC, ok := iroom.clients[c.userID]; ok {
-		activeC.conns[c] = connectionInfo{}
-		return
-	}
-
-	// create new active client because the connection is newly.
-	// and broadcasts user connect event to all active friends
-	activeC := newActiveClient(c)
-	// TODO set friends and rooms to active user.
-	iroom.clients[c.userID] = activeC
-	uc := UserConnect{} // TODO implement UserConncet.
-	iroom.broadcastsFriends(activeC, uc)
-}
-
-func (iroom *InitialRoom) broadcastsFriends(activeC *activeClient, m ActionMessage) {
-	for friendID, _ := range activeC.friends {
-		if activeFriend, ok := iroom.clients[friendID]; ok {
-			activeFriend.Send(m)
 		}
 	}
 }
@@ -139,25 +88,6 @@ type exitRoomRequest struct {
 	Client *Conn
 }
 
-func (iroom *InitialRoom) disconnectClient(c *Conn) {
-	activeC, ok := iroom.clients[c.userID]
-	if !ok {
-		return
-	}
-	connInfo, ok := activeC.conns[c]
-	if !ok {
-		return
-	}
-	iroom.roomManager.DisconnectClient(connInfo.currentRoomID, c)
-
-	delete(activeC.conns, c)
-	if len(activeC.conns) == 0 {
-		delete(iroom.clients, c.userID)
-		// udc := UserDisconnect{} // TODO implement UserDisconncet.
-		// iroom.broadcastsFriends(activeC, udc)
-	}
-}
-
 func (iroom *InitialRoom) handleMessage(ctx context.Context, m ActionMessage) {
 	switch m.Action() {
 	case ActionCreateRoom:
@@ -168,33 +98,12 @@ func (iroom *InitialRoom) handleMessage(ctx context.Context, m ActionMessage) {
 }
 
 func (iroom *InitialRoom) enterRoom(ctx context.Context, req enterRoomRequest) {
-	if err := iroom.validateClientHasRoom(req.Client, req.SenderID, req.RoomID); err != nil {
+	if err := iroom.clients.validateClientHasRoom(req.Client, req.SenderID, req.RoomID); err != nil {
 		sendError(req.Client, err)
 		return
 	}
 
-	iroom.roomManager.EnterRoom(ctx, req.CurrentRoomID, req.RoomID, req.Client)
-}
-
-func (iroom *InitialRoom) validateClientHasRoom(conn *Conn, userID, roomID uint64) error {
-	if !iroom.connectionExist(userID, conn) {
-		return fmt.Errorf("request user id(%d) is not found", userID)
-	}
-	// the client is validated at above.
-	if _, ok := iroom.clients[userID].rooms[roomID]; !ok {
-		return fmt.Errorf("user(%d) does not have request room id(%d) ", userID, roomID)
-	}
-	return nil
-}
-
-// check whether active client with the websocket connection exists?
-func (iroom *InitialRoom) connectionExist(userID uint64, conn *Conn) bool {
-	if activeC, ok := iroom.clients[userID]; ok {
-		if _, ok := activeC.conns[conn]; ok {
-			return true
-		}
-	}
-	return false
+	iroom.rooms.EnterRoom(ctx, req.CurrentRoomID, req.RoomID, req.Client)
 }
 
 func sendError(c *Conn, err error) {
