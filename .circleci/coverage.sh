38bf3df97d3ad19b153070c93e4d1a8cddcf7dca
mzki <mk22.cc328@gmail.com>
2017-11-19 02:33:38 +0900

    make coverage script is independent
    because the script uses coplicated shell expression which
    is not easily wrote for yaml file.

---
+++ .circleci/coverage.sh
@@ -0,0 +1,3 @@
+# collect coverage result on the circleci environment.
+GOTEST_COVERAGE_PKGS=`go list ./... | grep -v "/main\|/internal\|/wstest"`
+goverage -coverprofile=${TEST_RESULTS}/go-test.coverage ${GOTEST_COVERAGE_PKGS}
